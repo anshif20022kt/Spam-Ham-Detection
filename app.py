bd1649437704316e88f0f15d5115a6910e35cbc2
MUHAMMED ANSHIF KT <muhammedkt.ds24@duk.ac.in>
2025-05-22 12:07:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 import string
 
 # Load model
- with open("spam_classifier.pkl", "rb") as f:
+ with open("./spam_classifier.pkl", "rb") as f:  
         model = pickle.load(f)
 
 # Preprocessing function
